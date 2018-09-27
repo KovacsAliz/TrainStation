761bcd2ec719b561afeb6aeb94cc81e0a2e942b4
alice.kovacs <alice.kovacs@gmail.com>
2018-09-27 09:30:44 +0200

    Add project files.

---
+++ TrainStation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
